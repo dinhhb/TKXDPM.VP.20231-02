c30e22d167a4a4814ce22ac8f67595ca431ddac8
dinhhb <habuudinh@gmail.com>
2023-11-25 12:50:11 +0700

    Revert "fix junit test"
    This reverts commit 135543a009e4c8d17fe473e89e5f72f86834f82a.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,11 @@ module com.example.aims {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-//    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.api;
     requires java.sql;
 
-    opens hust.soict.itep.aims to javafx.fxml;
+    opens hust.soict.itep.aims.controller to javafx.fxml;
 //    opens hust.soict.itep.aims.entity to javafx.base;
-    exports hust.soict.itep.aims to javafx.graphics;
 
     exports hust.soict.itep.aims.view;
     opens hust.soict.itep.aims.entity.cart to javafx.base;
