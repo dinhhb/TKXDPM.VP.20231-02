0e31908b7905cce388c491148865590509dba986
DinhDuong <dinhduong1704bg@gmail.com>
2024-01-02 11:24:09 +0700

    add unit test

--- src/test/java/com/hust/itep/aims/controller/HomeControllerTest.java
+++ src/test/java/com/hust/itep/aims/controller/HomeControllerTest.java
@@ -1,17 +1,18 @@
-//package com.hust.itep.aims.controller;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class HomeControllerTest {
-//
-//    @Test
-//    void getAllMedia() {
-//        HomeController homeController = new HomeController();
-//        assertNotNull(homeController.getAllMedia());
-//    }
-//}
+package com.hust.itep.aims.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HomeControllerTest {
+
+    @Test
+    void getAllMedia() throws SQLException {
+        HomeController homeController = new HomeController();
+        assertNotNull(homeController.getAllMedia().size());
+    }
+}
